17a468e917335491fbbc010ee4f1dfb4361b9a8b
yoonjaehee <>
2022-06-22 14:05:57 +0900

    autotrace complete

---
+++ autotrace.sql
@@ -0,0 +1,10 @@
+create table t
+as
+select * from all_objects;
+update t set object_id = rownum;
+create unique index t_idx on t(object_id);
+analyze table t compute statistics;
+set autotrace traceonly explain
+select object_name from t where object_id = 1000;
+set autotrace off
+alter system flush shared_pool;
