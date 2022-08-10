760436a956a67b9ead20fbbe1c4d8d5a4dee60ea
yoonjaehee <46480817+yoonjaehee@users.noreply.github.com>
2022-08-10 10:36:46 +0900

    unnesting vs pushing....
    pushing => filter eliminate
    change where condition => eliminate table full scan

---
+++ unnesting vs pushing.sql
@@ -0,0 +1,11 @@
+set autotrace on explain;
+create index dept_x01 on dept(dname,deptno);
+create index emp_x01 on emp(deptno,job);
+select /*+ index(d dept_x01) */ * from dept d
+where dname = 'SALES'
+and exists (
+    select /*+ no_unnest no_push_subq */ 'x'
+    from emp
+    where deptno = d.deptno
+    and job = 'SALESMAN'
+    and sal>1000)
